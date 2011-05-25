b91356c67233004727f38376aab17efeffa0ea24
Ian Barber <ian.barber@gmail.com>
2011-05-25 22:42:29 +0100

    updating example to use multi

--- log/logserv.php
+++ log/logserv.php
@@ -5,10 +5,7 @@ $in = $ctx->getSocket(ZMQ::SOCKET_PULL);
 $in->bind("tcp://*:5555");
 
 while(true) {
-    $messages = array();
-    do {
-        $messages[] = $in->recv();
-    } while($in->getSockOpt(ZMQ::SOCKOPT_RCVMORE));
+    $messages = $in->recvMulti();
     // ... write the messages to disk or similar in one batch
     echo "Messages Received: ", count($messages), PHP_EOL;
     foreach($messages as $msg) {
